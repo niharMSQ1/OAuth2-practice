757a1948b6f8996807292ef0f15c91b35ec63d42
nihar ranjan mohanty <nihar.m@secqureone.com>
2024-01-08 18:01:44 +0530

    oauth2 for practice

---
+++ django_oauth2/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from oauth_test.views import UserList, UserDetails, GroupList
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('users/', UserList.as_view()),
+    path('users/<pk>/', UserDetails.as_view()),
+    path('groups/', GroupList.as_view()),
+]
